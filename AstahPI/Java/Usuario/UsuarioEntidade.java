fbc75f4d473f7555d21937ca9e4bf33dccb75d23
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-01 16:24:39 -0300

    Classe geradas pelo Astah

---
+++ AstahPI/Java/Usuario/UsuarioEntidade.java
@@ -0,0 +1,29 @@
+package Usuario;
+
+public class UsuarioEntidade {
+
+	private long id_usuario;
+
+	private String email;
+
+	private boolean status_conexao;
+
+	private String senha;
+
+	private String nome;
+
+	private long codigo_usuario;
+
+	public void getter() {
+
+	}
+
+	public void setter() {
+
+	}
+
+	public void gerarCodigo() {
+		return 0;
+	}
+
+}
---
+++ AstahPI/Java/Usuario/UsuarioRepositorio.java
@@ -0,0 +1,13 @@
+package Usuario;
+
+public class UsuarioRepositorio {
+
+	public UsuarioEntidade findByNome(String nome) {
+		return null;
+	}
+
+	public UsuarioEntidade findByEmail(String email) {
+		return null;
+	}
+
+}
