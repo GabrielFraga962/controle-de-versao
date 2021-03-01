adb8e4adcb0c0aaee7871cb883cd24ea5253729d
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-01 16:24:39 -0300

    Classe geradas pelo Astah

---
+++ AstahPI/Java/Usuario/UsuarioController.java
@@ -0,0 +1,25 @@
+package Usuario;
+
+public class UsuarioController {
+
+	public UsuarioEntidade findAllUsuarios() {
+		return null;
+	}
+
+	public UsuarioEntidade addUsuario(UsuarioEntidade usuario) {
+		return null;
+	}
+
+	public UsuarioEntidade findUsuarioById(int id_usuario) {
+		return null;
+	}
+
+	public UsuarioEntidade updateUsuario(int id_usuario, UsuarioEntidade usuario) {
+		return null;
+	}
+
+	public void deleteUsuario(int usuario_id) {
+
+	}
+
+}
---
+++ AstahPI/Java/Usuario/UsuarioService.java
@@ -0,0 +1,25 @@
+package Usuario;
+
+public class UsuarioService {
+
+	public boolean validaEmail(char email) {
+		return false;
+	}
+
+	public boolean validaNome(char nome) {
+		return false;
+	}
+
+	public UsuarioEntidade cadastraUsuario(UsuarioEntidade usuario) {
+		return null;
+	}
+
+	public void login(UsuarioEntidade Usuario) {
+
+	}
+
+	public void logout(UsuarioEntidade usuario) {
+
+	}
+
+}
